ac9efdd03a98658c452b6650af985e940c07d374
Koushik Biswas <Koushik.Biswas@in.ey.com>
2022-10-28 11:38:01 +0530

    navbar, and two forms modification

--- src/Home.js
+++ src/Home.js
@@ -41,11 +41,11 @@ const Home = () => {
         <h2 className="section-title">Choose Schema</h2>
         <div className="d-flex justify-content-center align-item-stretch flex-wrap">
           {CardData.map((value, index) => (
-            <div className="col-12 col-md-4 col-lg-4 p-2">
+            <div className="col-12 col-sm-6 col-md-4 col-lg-4 p-2">
 
               <div className="card text-center" key={index}>
                 <h5 className="card-title"> {value} </h5>
-                <div className="button-grp d-flex justify-content-center align-item-center h-100">
+                <div className="button-grp d-flex justify-content-center align-item-center mt-auto">
                   <button className="btn btn-orange" onClick={handleDownload}>Download</button>
                   <button className="btn btn-orange" onClick={() => handleCertify(index,value)}>Certify</button>
                 </div>
