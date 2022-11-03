0c0f1d48d574a99f92579801b74775fc624ceb44
Chaitanya Gupta <chg140100@gmail.com>
2022-11-04 01:02:26 +0530

    Update to Transaction Id call

--- src/components/OnSubmitCertify2.js
+++ src/components/OnSubmitCertify2.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { useState } from "react";
+//import { CertifyCall, transactionId } from "../service/generateTransactionId";
 
-import { CertifyCall } from "../service/generateTransactionId";
-
+import axios from "axios";
+import { SchemaName } from "../components/Home";
+// import { toast } from "react-toastify";
+// const API_URL = 'http://localhost:8013/certify/';
+const API_URL = "http://52.172.132.121/vc-certification/v1/certify/"
 
 const OnSubmitCertify2 = () => {
     // const [preEnrollmentCode, setPreEnrollmentCode] = useState("");
@@ -36,16 +40,16 @@ const OnSubmitCertify2 = () => {
     const [registrationExpiry,setRegistrationExpiry] = useState("");
     const [issuer,setIssuer] = useState("");
     const [issuanceDate,setIssuanceDate] = useState("");
-   
-    var transactionId;
-
+    const [transactionID,setTransactionID] = useState('');
+    
     const handleSubmit = (e) => {
+      
         e.preventDefault();
-        transactionId = certify()
-        console.log(transactionId);
+        certify();
       }
     
     const certify = () => {
+        
         // var data = JSON.stringify({
         // "preEnrollmentCode": preEnrollmentCode,
         // "certificateId": certificateId,
@@ -81,8 +85,36 @@ const OnSubmitCertify2 = () => {
           "issuer": "http://www.india.gov.in",
           "issuanceDate": "2021-09-09T21:01:01.121Z"
       })
-    
-        return CertifyCall(data2);
+
+
+      var config = {
+        method: 'post',
+        url: API_URL+SchemaName,
+  
+        data: data2,
+        headers: {
+          'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJxMmRGQXVDYWV3V0xpcUtYcHZ4dmxzczFvaWw0VkZJLWNfNlFmVXlKa1VJIn0.eyJleHAiOjE2OTkwMjgyNzgsImlhdCI6MTY2NzQ5MjI3OCwianRpIjoiMzI3MmQxNzAtOTAxYS00ZDQ4LTliMzItNmQyOTk4ZTRiNDMzIiwiaXNzIjoiaHR0cDovLzUyLjE3Mi4xMzIuMTIxL2F1dGgvcmVhbG1zL3N1bmJpcmQtcmMiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiODZmOWRkYjUtMGE5MS00NzUxLTg5MGMtOGFkNTkwOTQ2MGZhIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoicmVnaXN0cnktZnJvbnRlbmQiLCJzZXNzaW9uX3N0YXRlIjoiNTAwYzhiN2UtOTA0ZS00OTQzLTk2Y2UtZTQ4MjU3YTc0ZmU3IiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwczovL2xvY2FsaG9zdDo0MjAyIiwiaHR0cDovL2xvY2FsaG9zdDo0MjAyIiwiaHR0cHM6Ly9sb2NhbGhvc3Q6NDIwMCIsImh0dHBzOi8vbmRlYXIueGl2LmluIiwiaHR0cDovL2xvY2FsaG9zdDo0MjAwIiwiaHR0cDovL25kZWFyLnhpdi5pbiIsImh0dHBzOi8vc3VuYmlyZC1jZXJ0aWZpY2F0ZS1kZW1vLnhpdi5pbiIsImh0dHA6Ly8yMC4xOTguNjQuMTI4Il0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsImFkbWluIiwidW1hX2F1dGhvcml6YXRpb24iLCJkZWZhdWx0LXJvbGVzLW5kZWFyIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJwcm9maWxlIGVtYWlsIiwic2lkIjoiNTAwYzhiN2UtOTA0ZS00OTQzLTk2Y2UtZTQ4MjU3YTc0ZmU3IiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJ2aXNod2ExIiwiZW50aXR5IjpbIlRlbmFudCJdLCJlbWFpbCI6InZpc2h3YTEifQ.chXmv3MY2-6IkMVOBOoJJdSOxmY5WQ0JYdoU0WrOEp81h6F3ehNnfYfmkY4ofTTBL-i0I7VTvMd3JaPR7zUofSxTsoFayYZrMMhtavqN09t915SaXao6-LYLsg7sohFbGbaRcw9E1bQPpAQeN9dcPWTi7-CCrCoYoXPE8Rw814TDoQzq6uxdeEO4K6ZVOqawPDi1oTjwIt5O5TTMqgYbPN-3CRYpeCSPoEaDQxdyabyLAR9bk6nVuCZYKjHAOh9fxQinW_j44gBJuoOPsE-dd_LiS9tvIpsia5RxnSSwp83bc6Yy6dRQ3ZkSsVDQ4MahYSz6F-ZVPO3J-tY4lkL5RQ',
+          'Content-Type': 'application/json'
+        },
+      };
+      
+      axios(config)
+          .then((response) => {
+          if (response.data) {
+            // transactionID = response.data.transactionId;
+            let parsedResponse = JSON.parse(JSON.stringify(response.data));
+            //console.log(parsedResponse);
+            setTransactionID(parsedResponse.transactionId);
+            console.log(transactionID)
+            return response.data;
+          }
+        },
+        error=>{
+          // toast.error("Invalid Credentials!!");
+          console.log(error);
+        })
+
+        // return CertifyCall(data2);
       }
       
 
@@ -93,7 +125,7 @@ return (
       <div className="row d-flex justify-content-center">
           <div className="col-xl-10 col-lg-10 col-md-10 col-11 text-center mb-4 flex-column">
               <h2 className="font-weight-bold ml-md-0 mx-auto text-center text-sm-left mt-3 mb-2 form-title">NHAUIP Certificate</h2>   
-              {/* <p className="font-weight-bold ml-md-0 mx-auto text-center text-sm-left mt-0 mb-3 form-title">Transaction id is: {transactionId}</p>          */}
+              <p className="font-weight-bold ml-md-0 mx-auto text-center text-sm-left mt-0 mb-3 form-title">Transaction id is: {transactionID}</p>         
               <div className="card box-form">
                   <form className="form-card" onSubmit={handleSubmit}>
                       <div className="row justify-content-between text-left">
