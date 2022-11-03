312e571506740e426392cc9c9c952f7dd07d5415
Chaitanya Gupta <chg140100@gmail.com>
2022-11-03 20:01:59 +0530

    required field

--- src/components/OnSubmitDownload.js
+++ src/components/OnSubmitDownload.js
@@ -39,9 +39,10 @@ return(
                         <form className="form-card">
                             <div className="row justify-content-between text-left">
                                 <div className="form-group col-12 col-md-6 col-lg-3 flex-column d-flex"> 
-                                    <label className="form-control-label px-0">Transaction Id:</label> 
+                                    <label className="form-control-label px-0">Certificate Id</label> 
                                     <input 
-                                    type="text" 
+                                    type="text"
+                                    required 
                                     className="form-control" 
                                     value= {CertificateId}
                                     onChange={(e) => setCertificateId(e.target.value)}
